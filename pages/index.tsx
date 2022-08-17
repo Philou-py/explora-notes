a2805e88c2cae7916f2d531fb3e07ad626c71a96
Philippe Schoenhenz <philippe.schoenhenz@gmail.com>
2022-08-17 10:56:58 +0200

    🎉 (Whole App): Initialise ExploraNotes with its dependencies

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import cn from "classnames/bind";
+import pageStyles from "../pageStyles/Home.module.scss";
+
+const cx = cn.bind(pageStyles);
+
+export default function Home() {
+  return <h1>Welcome to ExploraNotes!</h1>;
+}
