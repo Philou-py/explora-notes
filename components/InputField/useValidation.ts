afc29b250dcdcc74d91cc1c253f807bd8ebdd754
Philippe Schoenhenz <philippe.schoenhenz@gmail.com>
2022-10-05 18:01:33 +0200

    🐛 (useValidation.ts): Fix email address regex by adding the dash in tld

--- components/InputField/useValidation.ts
+++ components/InputField/useValidation.ts
@@ -30,7 +30,7 @@ export default function useValidation(
     } else {
       // Default validation (email)
       let defaultValidationPassed = true;
-      const emailRegex = /^[a-z]+(\.[a-z]+)?@[a-z]+\.[a-z]+(\.[a-z]+)?$/;
+      const emailRegex = /^[a-z]+(\.[a-z]+)?@[a-z-]+\.[a-z]+(\.[a-z]+)?$/;
       if (inputType === "email" && !emailRegex.test(inputValue)) {
         setMessage("Invalid email");
         defaultValidationPassed = false;
