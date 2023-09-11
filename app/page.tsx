8d413ae6c9e1b157b96117a0be8d6102cb48279a
Philippe Schoenhenz <philippe.schoenhenz@gmail.com>
2023-09-11 21:42:42 +0200

    💬 (Home pages): Final fix to the app's description

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         <p className="text-justify">
           Cette application s&rsquo;adresse aux professeurs à la recherche d&rsquo;un outil
           informatique de saisie, de synthèse et d&rsquo;archivage des notes de leurs élèves. Elle
-          permet de créer ses groupes d&rsquo;élèves, définir le barème de ses évaluations et de
+          permet de créer ses groupes d&rsquo;élèves, de définir le barème de ses évaluations et de
           saisir les résultats détaillés de chaque copie.
         </p>
         <p className="text-justify">
--- app/teacher/[teacherEmail]/page.tsx
+++ app/teacher/[teacherEmail]/page.tsx
@@ -3,11 +3,11 @@ import Container from "@/components/Container";
 export default function TeacherHome() {
   return (
     <Container>
-      <h1 className="pageTitle text-center">Bienvenue dans ExploraNotes!</h1>
+      <h1 className="pageTitle text-center">Bienvenue dans ExploraNotes !</h1>
       <p className="text-justify">
         Cette application s&rsquo;adresse aux professeurs à la recherche d&rsquo;un outil
         informatique de saisie, de synthèse et d&rsquo;archivage des notes de leurs élèves. Elle
-        permet de créer ses groupes d&rsquo;élèves, définir le barème de ses évaluations et de
+        permet de créer ses groupes d&rsquo;élèves, de définir le barème de ses évaluations et de
         saisir les résultats détaillés de chaque copie.
       </p>
       <p className="text-justify">
