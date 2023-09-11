1f625d6868d09dac1a9b8614412ceb28c8c45c9c
Philippe Schoenhenz <philippe.schoenhenz@gmail.com>
2023-09-11 21:14:17 +0200

    💬 (App description): Update app description + fix other texts

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { readFileSync } from "fs";
 const publicKey = readFileSync("public.key");
 
 export const metadata: Metadata = {
-  title: "ExploraNotes - Page d'accueil",
+  title: "ExploraNotes - Page d’accueil",
 };
 
 function checkAccountType() {
@@ -30,13 +30,17 @@ export default function Home() {
     <>
       <NavBar />
       <Container>
-        <h1 className="pageTitle text-center">Bienvenue dans ExploraNotes!</h1>
+        <h1 className="pageTitle text-center">Bienvenue dans ExploraNotes !</h1>
         <p className="text-justify">
           Cette application s&rsquo;adresse aux professeurs à la recherche d&rsquo;un outil
           informatique de saisie, de synthèse et d&rsquo;archivage des notes de leurs élèves. Elle
-          permet de créer ses groupes d&rsquo;élèves, définir le barème de ses évaluations, saisir
-          les résultats détaillés de chaque copie, consulter les moyennes de classes et les moyennes
-          individuelles.
+          permet de créer ses groupes d&rsquo;élèves, définir le barème de ses évaluations et de
+          saisir les résultats détaillés de chaque copie.
+        </p>
+        <p className="text-justify">
+          Une fois les copies corrigées et les résultats publiés, les élèves possédant un compte
+          peuvent rapidement consulter le détail de leurs notes. Pour rejoindre un groupe, rien de
+          plus simple : il suffit de scanner un QR Code fourni par le professeur.
         </p>
         <p className="text-justify">
           Ce site a été créé pour répondre à la demande d&rsquo;un professeur. Son créateur sera
--- app/student/[studentEmail]/page.tsx
+++ app/student/[studentEmail]/page.tsx
@@ -3,13 +3,17 @@ import Container from "@/components/Container";
 export default function StudentHome() {
   return (
     <Container>
-      <h1 className="pageTitle text-center">Elève, Bienvenue dans ExploraNotes!</h1>
+      <h1 className="pageTitle text-center">Élève, Bienvenue dans ExploraNotes !</h1>
       <p className="text-justify">
         Cette application s&rsquo;adresse aux professeurs à la recherche d&rsquo;un outil
         informatique de saisie, de synthèse et d&rsquo;archivage des notes de leurs élèves. Elle
-        permet de créer ses groupes d&rsquo;élèves, définir le barème de ses évaluations, saisir les
-        résultats détaillés de chaque copie, consulter les moyennes de classes et les moyennes
-        individuelles.
+        permet de créer ses groupes d&rsquo;élèves, définir le barème de ses évaluations et de
+        saisir les résultats détaillés de chaque copie.
+      </p>
+      <p className="text-justify">
+        Une fois les copies corrigées et les résultats publiés, les élèves possédant un compte
+        peuvent rapidement consulter le détail de leurs notes. Pour rejoindre un groupe, rien de
+        plus simple : il suffit de scanner un QR Code fourni par le professeur.
       </p>
       <p className="text-justify">
         Ce site a été créé pour répondre à la demande d&rsquo;un professeur. Son créateur sera ravi
